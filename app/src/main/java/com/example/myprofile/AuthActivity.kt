d2aa61725d705fc15cb149b05ce5676efb31471a
VladislawQQ <vladikstrun48@gmail.com>
2023-04-09 19:52:14 +0300

    update

--- app/src/main/java/com/example/myprofile/AuthActivity.kt
+++ app/src/main/java/com/example/myprofile/AuthActivity.kt
@@ -2,21 +2,24 @@ package com.example.myprofile
 
 import android.annotation.SuppressLint
 import android.app.ActivityOptions
-import android.content.Context
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.Color.red
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.red
 import androidx.core.widget.doAfterTextChanged
 import com.example.myprofile.databinding.ActivityAuthBinding
 import com.google.android.material.snackbar.Snackbar
 
 @SuppressLint("StaticFieldLeak")
 private lateinit var binding: ActivityAuthBinding
+private lateinit var sharedPreferences: SharedPreferences
+
+private const val KEY_EMAIL = "email"
+private const val KEY_PASSWORD = "password"
+private const val KEY_REMEMBERME = "rememberMe"
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,29 +27,83 @@ class AuthActivity : AppCompatActivity() {
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Create SharedPreferences xml file
+        sharedPreferences = this.getSharedPreferences("autoLogin", MODE_PRIVATE)
+
+        // Check if "remember me" is true
+        // then autologin to profile and start "my profile" intent
+        if (sharedPreferences.getBoolean("rememberMe", false)) {
+            binding.emailEditText.setText(sharedPreferences.getString("email", ""))
+            binding.passwordEditText.setText(sharedPreferences.getString("password", ""))
+
+            intent = Intent(this, ProfileActivity::class.java)
+
+            intent.putExtra("usersEmail", binding.emailEditText.text.toString())
+
+            startActivity(intent)
+        }
+
+        // Email and password validation
         emailErrorChanges()
         passwordErrorChanges()
 
+        // Check if fields with email or password is valid
+        // then start "my profile" intent
+        // else show Snackbar
         binding.registerButton.setOnClickListener {
             if (!fieldsIsEmpty() &&
                 binding.passwordEditText.text.toString() != "" &&
                 binding.emailEditText.text.toString() != "") {
 
-                intent = Intent(this, ProfileActivity:: class.java)
-                startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+                if (binding.rememberMeCheckbox.isChecked) {
+                    putDataToSharedPreferences()
+                }
+
+                // init and start profile activity with anim
+                intent = Intent(this, ProfileActivity::class.java)
+
+                intent.putExtra("usersEmail", binding.emailEditText.text.toString())
+
+                startActivity(intent,
+                    ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
             } else {
                 val viewColor = ContextCompat.getColor(this, R.color.view_color)
 
-                val snackBar: Snackbar = Snackbar.make(binding.root, "Not all fields are filled", Snackbar.LENGTH_LONG)
-                    .setTextColor(viewColor)
+                val snackBar: Snackbar = Snackbar.make(
+                    binding.root,
+                    "Not all fields are filled",
+                    Snackbar.LENGTH_LONG
+                ).setTextColor(viewColor)
 
-                snackBar.setBackgroundTint(ContextCompat.getColor(this, R.color.white_edit_text))
+                snackBar.setBackgroundTint(
+                    ContextCompat.getColor(
+                        this,
+                        R.color.white_edit_text
+                    )
+                )
 
                 snackBar.show()
             }
         }
     }
 
+
+    /**
+     * Put email, password and boolean rememberMe to sharedPreferences xml file
+     */
+    private fun putDataToSharedPreferences() {
+        val editor = sharedPreferences.edit()
+        editor.putString(KEY_EMAIL, binding.emailEditText.text.toString())
+        editor.putString(KEY_PASSWORD, binding.passwordEditText.text.toString())
+        editor.putBoolean(KEY_REMEMBERME, true)
+        editor.apply()
+    }
+
+    /**
+     * Check email and password container.error
+     * if null  return false
+     * else true
+     */
     private fun fieldsIsEmpty(): Boolean {
         val validEmail = binding.emailContainer.error == null
         val validPassword = binding.passwordContainer.error == null
@@ -60,10 +117,15 @@ class AuthActivity : AppCompatActivity() {
 
     private fun emailErrorChanges() {
         binding.emailEditText.doAfterTextChanged {
-                binding.emailContainer.error = emailIsValid()
+            binding.emailContainer.error = emailIsValid()
         }
     }
 
+    /**
+     * Check if email is valid with Patterns.EMAIL_ADDRESS
+     * if !isValid return "Invalid Email Address"
+     * else return null
+     */
     private fun emailIsValid(): String? {
         val emailText = binding.emailEditText.text.toString()
 
@@ -77,9 +139,15 @@ class AuthActivity : AppCompatActivity() {
 
     private fun passwordErrorChanges() {
         binding.passwordEditText.doAfterTextChanged {
-            binding.passwordContainer.error = passwordIsValid() }
+            binding.passwordContainer.error = passwordIsValid()
+        }
     }
 
+    /**
+     * Check if password is valid with regex
+     * if !isValid return String
+     * else return null
+     */
     private fun passwordIsValid(): String? {
         val passwordText = binding.passwordEditText.text.toString()
 
@@ -97,3 +165,5 @@ class AuthActivity : AppCompatActivity() {
         } else null
     }
 }
+
+
--- app/src/main/java/com/example/myprofile/ProfileActivity.kt
+++ app/src/main/java/com/example/myprofile/ProfileActivity.kt
@@ -1,12 +1,38 @@
 package com.example.myprofile
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myprofile.databinding.ActivityAuthBinding
+import com.example.myprofile.databinding.ActivityProfileBinding
+import java.util.regex.Pattern
+
+@SuppressLint("StaticFieldLeak")
+private lateinit var binding: ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val arguments = intent.extras
+
+        val email = arguments?.get("usersEmail").toString()
+        val parsedName = parseEmail(email)
+
+        val name = parsedName[0].substring(0,1).uppercase() + parsedName[0].substring(1).lowercase()
+        val surname = parsedName[1].substring(0,1).uppercase() + parsedName[1].substring(1).lowercase()
+
+        binding.profileNameTextView.text = "$name $surname"
+    }
+
+    private fun parseEmail(email: String): List<String> {
+
+        return "@.*\$".toRegex().replace(email, "")
+            .split(".")
     }
 }
